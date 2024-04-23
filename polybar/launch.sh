9e076f172822f35bb9cf98e1557c73d0c65ac51c
Dmytro Sokol <dfalcon.7c8@gmail.com>
2024-04-23 13:08:17 +0300

    some changes

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1 | sort); do
     MONITOR=$m polybar --reload example &
   done
 else
